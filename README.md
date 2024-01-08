7d72ed4aec2e801a1aa8f40c178b9a5fc9e8656d
sonustar <91054530+sonustar@users.noreply.github.com>
2024-01-08 11:22:12 +0000

    We added the marker

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Using Leaflet : Open source library for maps
 
 Understanding leaflet !!
 
+// Night Task :
+1. Make several polygons 
+2. Take the coordinates of the forts 
+3. Onlicking we get a pop-up 
+4. Showing the picture of the fort 
--- script.js
+++ script.js
@@ -10,9 +10,50 @@ const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Op
 
 // i need to add it to the map :
 const Tilelayer = L.tileLayer(tileurl, {
-    attribution:attribution
+    attribution:attribution,
+    
 })
 
 Tilelayer.addTo(map);
 
 // Added the map in our file .
+
+// Adding a circle on the map : 
+const circle =L.circle([51.505, -0.09], {
+    radius: 200 ,
+     color : "red",
+     interactive : false
+ })
+
+circle.addTo(map);
+
+const bt = [
+   [
+    [25.7617, -80.1918],
+    [32.3078, -64.7505], 
+    [18.4655, -66.1057]
+   ]
+]
+const btpolygon = L.polygon(bt,{
+    color:'red'
+})
+btpolygon.addTo(map)
+
+// polyLine can be used to show the routes between 2 places 
+
+//marker use karsakte hoo to indicate a certain [latitude,longitude]
+// L.marker([22.5726, 88.3639]).addTo(map);
+// L.marker([22.5726, 88.3639]);
+
+// kolkata.addTo(map)
+
+map.on('click', function(ev) {
+    // alert(ev.latlng); // ev is an event object (MouseEvent in this case)
+    var t = ev.latlng
+    // console.log(t);
+    // console.log(t.lat)
+L.marker([t.lat, t.lng]).addTo(map);
+
+});
+
+// we are using the mouse and marking the places .
